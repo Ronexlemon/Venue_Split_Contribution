5c9164e9f6712f084e9beaef0e1568a48e8de8c2
RonexLemon <ronexondimu@gmail.com>
2023-01-12 14:42:27 +0300

    include check function fr rsvp

--- contracts/party.sol
+++ contracts/party.sol
@@ -5,6 +5,7 @@ contract Party {
     uint rsvpAmount;
     uint venueAmount;
     address[] groupMembers;
+    bool public status;
     //maping to store the address of the already rsvp members
     mapping(address => bool) public hasRsvp;
 
@@ -14,28 +15,42 @@ contract Party {
 
     //function to rsvp
     function rsvp() external payable {
-        require(msg.value == rsvpAmount, "less account balance");
+        require(msg.value >= rsvpAmount, "less account balance");
         require(!hasRsvp[msg.sender], "already rsvp to join the party");
-        (bool success, ) = address(this).call{value: msg.value}("");
-        require(success);
+        // (bool success, ) = address(this).call{value: rsvpAmount}("");
+        // require(success, "failed");
+        payable(address(this)).transfer(msg.value);
         hasRsvp[msg.sender] = true;
+        status = true;
     }
-    //paybill for the party
-    function payBill(address venue,uint Amount) external payable{
-        venueAmount = Amount;
-        (bool sent,) = venue.call{value: Amount}("");
-        require(sent,"no transaction sent");
-        uint amountToReturn = address(this).balance /groupMembers.length;
-        for(uint i; i< groupMembers.length; i++){
-            (bool returnAmount,) = groupMembers[i].call{value:amountToReturn}("");
-            require(returnAmount,"no transaction sent");
-        }
 
+    //paybill for the party
+    function payBill(address venue, uint Amount) external payable {
+        venueAmount = Amount;
+        (bool sent, ) = venue.call{value: Amount}("");
+        require(sent, "no transaction sent");
+        uint amountToReturn = address(this).balance / groupMembers.length;
+        for (uint i; i < groupMembers.length; i++) {
+            (bool returnAmount, ) = groupMembers[i].call{value: amountToReturn}(
+                ""
+            );
+            require(returnAmount, "no transaction sent");
+        }
     }
-    function returnAmountToPay()public view returns(uint){
+
+    function returnAmountToPay() public view returns (uint) {
         return rsvpAmount;
     }
-    function returnVenueAmount()public view returns(uint){
+
+    function returnVenueAmount() public view returns (uint) {
         return venueAmount;
     }
+
+    function returnStatus() public view returns (bool) {
+        return status;
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
--- test/party.js
+++ test/party.js
@@ -1,14 +1,29 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Party Contract",function(){
+describe("Party Contract", function(){
+    let amountTocontribute;
+    let PartyContractDeploy;
     it("deployment should assign the amount eact member should contribute", async function(){
-        const amountTocontribute = ethers.utils.parseEther("1");
+        const {owner, other} = ethers.getSigner();
+         amountTocontribute = ethers.utils.parseEther("1");
         const PartyContract = await ethers.getContractFactory("Party");
-        const PartyContractDeploy = await PartyContract.deploy(amountTocontribute);
+        PartyContractDeploy = await PartyContract.deploy(amountTocontribute);
         await PartyContractDeploy.deployed();
+        
         const amountForEachMember = await PartyContractDeploy. returnAmountToPay();
         expect(amountTocontribute.toString()).to.equal(amountForEachMember.toString());
+        
+        
     });
+     it("should check if a member could be able rsvp",async function(){
+        
+         await PartyContractDeploy.rsvp({value:amountTocontribute});
+         const status = await PartyContractDeploy.returnStatus();
+       
+        expect(status).to.equal(true);
+        
+       })
+       
     
 })
